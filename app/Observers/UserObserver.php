687c3a39237877f63c8e1d7056203449e33d2c80
mzjmc <mzjmc123@163.com>
2018-09-23 10:44:55 +0000

    Topic index page

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,19 +2,19 @@
 
 namespace App\Observers;
 
-use App\Models\User;
+use App\Models\Users;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class UserObserver
 {
-    public function creating(User $user)
+    public function creating(Users $user)
     {
         //
     }
 
-    public function updating(User $user)
+    public function updating(Users $user)
     {
         //
     }
--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -2,18 +2,18 @@
 
 namespace App\Policies;
 
-use App\Models\User;
+use App\Models\Users as User;
 use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
-    public function update(User $user, Topic $topic)
+    public function update(Users $user, Topic $topic)
     {
         // return $topic->user_id == $user->id;
         return true;
     }
 
-    public function destroy(User $user, Topic $topic)
+    public function destroy(Users $user, Topic $topic)
     {
         return true;
     }
