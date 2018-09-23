9befeffc096263ce003142205f183c5a91e373b1
mzjmc <mzjmc123@163.com>
2018-09-23 10:51:17 +0000

    Topic index page

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,13 +7,13 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
-    public function update(Users $user, Topic $topic)
+    public function update(User $user, Topic $topic)
     {
         // return $topic->user_id == $user->id;
         return true;
     }
 
-    public function destroy(Users $user, Topic $topic)
+    public function destroy(User $user, Topic $topic)
     {
         return true;
     }
